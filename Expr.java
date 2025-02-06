cb3de0c536f20616a49d3989459ffe683f1d5a95
Ashwin <ashwinjain101010@gmail.com>
2025-02-06 20:10:06 +0530

    Expr class definition

---
+++ Expr.java
@@ -0,0 +1,56 @@
+import java.util.List;
+abstract class Expr{
+	interface Visitor<R>{
+		R visitBinaryExpr(Binary expr);
+		R visitGroupingExpr(Grouping expr);
+		R visitLiteralExpr(Literal expr);
+		R visitUnaryExpr(Unary expr);
+	}
+	abstract <R> R accept(Visitor<R> visitor);
+	static class Binary extends Expr{
+		Binary(Expr l, Token op, Expr r){
+			this.l = l;
+			this.op = op;
+			this.r = r;
+		}
+		@Override
+		<R> R accept(Visitor<R> visitor){
+			return visitor.visitBinaryExpr(this);
+		}
+		final Expr l;
+		final Token op;
+		final Expr r;
+	}
+	static class Grouping extends Expr{
+		Grouping(Expr expr){
+			this.expr = expr;
+		}
+		@Override
+		<R> R accept(Visitor<R> visitor){
+			return visitor.visitGroupingExpr(this);
+		}
+		final Expr expr;
+	}
+	static class Literal extends Expr{
+		Literal(Object val){
+			this.val = val;
+		}
+		@Override
+		<R> R accept(Visitor<R> visitor){
+			return visitor.visitLiteralExpr(this);
+		}
+		final Object val;
+	}
+	static class Unary extends Expr{
+		Unary(Token op, Expr r){
+			this.op = op;
+			this.r = r;
+		}
+		@Override
+		<R> R accept(Visitor<R> visitor){
+			return visitor.visitUnaryExpr(this);
+		}
+		final Token op;
+		final Expr r;
+	}
+}
---
+++ generateAST.py
@@ -0,0 +1,42 @@
+base = "Expr"
+outputDir = "C:/jlox"
+path = outputDir + "/" + base + ".java"
+types = {
+    "Binary": "Expr l, Token op, Expr r",
+    "Grouping": "Expr expr",
+    "Literal": "Object val",
+    "Unary": "Token op, Expr r"
+}
+
+with open(path, "w") as out:
+    out.write("import java.util.List;\n")
+    out.write("abstract class " + base + "{\n")
+
+    #define visitor class
+    out.write("\tinterface Visitor<R>{\n")
+    for typeName, fields in types.items():
+        out.write("\t\tR visit" + typeName + base + "(" + typeName + " " + base.lower() + ");\n")
+    out.write("\t}\n")
+    #abstract accept definition
+    out.write("\tabstract <R> R accept(Visitor<R> visitor);\n")
+
+    #types 
+    for className, fields in types.items():
+        out.write("\tstatic class " + className + " extends " + base + "{\n")
+        #constructor
+        out.write("\t\t" + className + "(" + fields + "){\n")
+        fieldsList = fields.split(", ")
+        for field in fieldsList:
+            name = field.split()[1]
+            out.write("\t\t\tthis." + name + " = " + name + ";\n")
+        out.write("\t\t}\n")
+        #accept method
+        out.write("\t\t@Override\n")
+        out.write("\t\t<R> R accept(Visitor<R> visitor){\n")
+        out.write("\t\t\treturn visitor.visit" + className + base + "(this);\n")
+        out.write("\t\t}\n")
+        #data fields
+        for field in fieldsList:
+            out.write("\t\tfinal " + field + ";\n")
+        out.write("\t}\n")
+    out.write("}")
