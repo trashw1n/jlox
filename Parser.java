ef89c81c8a340b79b078d1c24dfb2c226ea727d5
Ashwin <ashwinjain101010@gmail.com>
2025-02-23 18:28:01 +0530

    implement for-loop statements

--- Parser.java
+++ Parser.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Parser {
@@ -39,11 +40,37 @@ public class Parser {
         return new Stmt.Var(name, initializer); 
     }
     private Stmt statement(){
+        if(match(TokenType.FOR)) return forStatement();
         if(match(TokenType.IF)) return ifStatement();
         if(match(TokenType.WHILE)) return whileStatement();
         if(match(TokenType.PRINT)) return printStatement();
         if(match(TokenType.LEFT_BRACE)) return new Stmt.Block(block());
         return expressionStatement();
+    }
+    private Stmt forStatement(){
+        consume(TokenType.LEFT_PAREN, "Expected '(' after 'for'.");
+        Stmt initializer = null;
+        if(match(TokenType.SEMICOLON)) initializer = null;
+        else if(match(TokenType.VAR)) initializer = varDeclaration();
+        else initializer = expressionStatement();
+        Expr condition = null;
+        if(!check(TokenType.SEMICOLON)) condition = expression();
+        consume(TokenType.SEMICOLON, "Expected ';' after loop condition");
+        Expr increment = null;
+        if(!check(TokenType.RIGHT_PAREN)) increment = expression();
+        consume(TokenType.RIGHT_PAREN, "Expected closing ')' after 'for' clauses.");
+        Stmt body = statement();
+        if(increment != null){
+            body = new Stmt.Block(Arrays.asList(body, new Stmt.Expression(increment)));
+        }
+        if(condition == null){
+            condition = new Expr.Literal(true);
+        }
+        body = new Stmt.While(condition, body);
+        if(initializer != null){
+            body = new Stmt.Block(Arrays.asList(initializer, body));
+        }
+        return body;
     }
     private Stmt whileStatement(){
         consume(TokenType.LEFT_PAREN, "Expected '(' after 'while'.");
